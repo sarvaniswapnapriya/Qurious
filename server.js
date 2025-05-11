dbce63afc353d7d262f379c7303f1ec4b168d861
Y Sarvani Swapna Priya <48046474+sarvaniswapnapriya@users.noreply.github.com>
2025-05-11 23:24:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -265,7 +265,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 3000;
 
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
